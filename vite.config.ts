c6fb558f2c86acbc0b96cba0a7a0f2ba1a03218d
Tanu123T <162577217+Tanu123T@users.noreply.github.com>
2026-01-06 16:49:30 +0530

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/TEAMPIXELL/', 
+  base: '/', 
   plugins: [react()],
 
   resolve: {
@@ -53,7 +53,7 @@ export default defineConfig({
 
   build: {
     target: 'esnext',
-    outDir: 'build',
+    outDir: 'dist',
   },
 
   server: {
