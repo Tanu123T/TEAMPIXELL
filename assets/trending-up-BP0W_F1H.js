2ab13ba24e37e775383fe69f2cc457312774df00
Tanu123T <162577217+Tanu123T@users.noreply.github.com>
2026-01-11 16:45:33 +0530

    Updates

--- assets/trending-up-BP0W_F1H.js
+++ assets/trending-up-BP0W_F1H.js
@@ -1,4 +1,4 @@
-import{d as n}from"./index-BQWcNM4s.js";/**
+import{c as n}from"./index-DvWsv9fz.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
